db184443cf0fc5c841e62eea94a321c575d4d5e4
Andrew Reis <84384871+eoandrw@users.noreply.github.com>
2022-06-07 10:48:24 -0300

    Update 🆕

---
+++ MVC/Model/init.php
@@ -0,0 +1,12 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR', 'localhost');
+define('BD_USUARIO', 'id18909336_php_user');
+define('BD_SENHA', 'uma_S3NH4_qualquer');
+define('BD_BANCO', 'id18909336_php_db');
