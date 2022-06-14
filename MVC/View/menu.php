c3765480435617d7b6a29badeaf75cfd35d65aed
Andrew Reis <84384871+eoandrw@users.noreply.github.com>
2022-06-14 11:09:16 -0300

    Update 🆕

--- MVC/View/menu.php
+++ MVC/View/menu.php
@@ -1,7 +1,7 @@
 <hr>
 <div class="form-group">
   <div class="mx-auto" style="width: 1200px;">
-    <a href="cadastro.php" class="btn btn-sucesses">Cadastrar</a>
+    <a href="form_cadastro.php" class="btn btn-sucesses">Cadastrar</a>
     <a href="index.php" class="btn btn-default">Consultar</a>
   </div>
 </div>
