f7e6a387f7752843900c3e14954b9c28230fd759
Andrew Reis <84384871+eoandrw@users.noreply.github.com>
2022-06-14 10:49:46 -0300

    Update 🆕

--- MVC/Model/banco.php
+++ MVC/Model/banco.php
@@ -5,7 +5,7 @@ require_once("init.php");
     class Banco
     {
         protected $mysqli;
-        
+    
     public function __construct()
     {
         echo "Conexão efetuada com sucesso.";
@@ -14,5 +14,16 @@ require_once("init.php");
     private function conexao()
     {
         $this->mysqli = new mysqli(BD_SERVIDOR, BD_USUARIO, BD_SENHA, BD_BANCO);
+    }
+    
+    public function setLivro ($nome,$autor,$quantidade,$preco,$data)
+    { 
+        $stmt = $this->mysqli->prepare("INSERTINTOlivros(`nome`,`autor`,`quantidade`,`preco`,`data`)VALUES(?,?,?,?,?)");
+        $stmt->bind_param("sssss",$nome,$autor,$quantidade,$preco,$data);
+        if ($stmt->execute() == TRUE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
