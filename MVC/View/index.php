3d7093f909bf3c7bbaba78d02c5d612a56e2c02f
Andrew Reis <84384871+eoandrw@users.noreply.github.com>
2022-06-29 21:47:31 -0300

    Updated old files 📁

--- MVC/View/index.php
+++ MVC/View/index.php
@@ -1,25 +1,33 @@
-<?php require_once("../model/banco.php"): ?>
-<DOCTYPE html>
+<!-- A função require_once() é usada para integrar um arquivo PHP em outro -->
+<?php require_once("../Model/banco.php"); ?>
+<?php require_once("../Controller/ControllerListar.php"); ?>
+
+<!DOCTYPE html>
 <html lang="pt-br">
+
 <?php include("head.php"); ?>
-<body> 
-<?php new Banco(); ?>
-  <?php include("menu.php"); ?>
+
+<body>
+    <?php new banco(); ?>
+    <?php include("menu.php"); ?>
     <table class="table">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Autor</th>
-          <th>Quantidade de Páginas</th>
-          <th>Preço</th>
-          <th>Data</th>
-          <th>Flag</th>
-          <th>Opções</th>
-        </tr>
-      </thead>
+
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Autor</th>
+                <th>Quantidade de Páginas</th>
+                <th>Preço</th>
+                <th>Data</th>
+                <th>Flag</th>
+                <th>Opções</th>
+            </tr>
+        </thead>
+        <!--passo 3 chamar o método listar registros cadastrados no banco-->
+        <tbody>
+            <?php new listarController(); ?>
+        </tbody>
     </table>
 </body>
 
 </html>
-
-
